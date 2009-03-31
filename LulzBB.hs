ce11c375b73156fafd3db153a176af7033508bdf
taro <qudesu@gmail.com>
2009-03-30 22:23:19 -0400

    Added in some code to deal with some URL oddities, namely, having a baseurl (since I'm testing it in a subdirectory), handling relative requests for "/", requests for "/something/" and such. Seems to handle it all pretty well now, though most of this shit needs to be refactored into the Router's code.

--- LulzBB.hs
+++ LulzBB.hs
@@ -1,5 +1,7 @@
 module Main where
+import Control.Exception (handle, Exception (NoMethodError))
 import Control.Monad (liftM)
+import Data.List (isPrefixOf)
 import Network.CGI
 import Network.URI (uriPath)
 import LulzBB.Main
@@ -14,9 +16,35 @@ main = do
 handler :: UrlTree -> ActionTable -> CGI CGIResult
 handler ut at = do
 	setHeader "Content-Type" "text/plain"
+
+	-- TODO: Un-hardcode this
+	-- TODO: Make sure this is /-terminated
+	let baseurl = "/lulzbb/"
 	
 	-- Resolve the action
-	req <- liftM uriPath requestURI
+	fullurl <- liftM uriPath requestURI
+
+	if not $ isPrefixOf baseurl fullurl
+		then throwCGI $ NoMethodError $ unlines [
+			"uri isn't in baseurl.",
+			"uri: " ++ fullurl, 
+			"baseurl: " ++ baseurl ]
+		else return ()
+
+	let req = (drop (length baseurl) fullurl)
+
+	-- TODO: Refactor the URL shit into the Router
+
+	-- chop off a trailing '/', if one exists
+	req <- return $ if (not $ null req) && (last req == '/')
+		then init req
+		else req
+
+	-- add a leading '/', unless one exists
+	req <- return $ if (null req) || (head req /= '/')
+		then '/' : req
+		else req
+	
 	let action = route ut at req
 
 	-- Execute the action
