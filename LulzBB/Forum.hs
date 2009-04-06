929873aa83864f510a295beba75c0047d3b4e1a8
taro <qudesu@gmail.com>
2009-04-05 21:11:24 -0400

    Implementing Forum.list; working with the ORM actually isn't that painful so far; saves me a shitton of fromMaybe/fromSql, which is nice (and what I wrote it for). I just realized I have to write JSON encoders for all the record types and am going back to reading Ranma now fucking Haskell sometimes you piss me off >:(

--- LulzBB/Forum.hs
+++ LulzBB/Forum.hs
@@ -1,6 +1,15 @@
 module LulzBB.Forum where
+import Control.Monad (liftM)
 import Database.HDBC
+import LulzBB.ORM
 import Routing (RouteParameters)
+
+list :: IConnection a => a -> RouteParameters -> IO String
+list db params = do
+	stm <- prepare db "SELECT * FROM s_forum"
+	execute stm []
+	fs <- liftM (map parseSql) $ fetchAllRowsMap stm :: IO [Maybe Forum]
+	return $ show fs
 
 view :: IConnection a => a -> RouteParameters -> IO String
 view db params = return "view forum"
