d9619f4b84971ad8ac204ce24e4551f4937ad5fc
taro <qudesu@gmail.com>
2009-04-06 21:43:14 -0400

    Changed the routing so that "/" points to Forum.list rather than Index.view; implemented Forum.list for a JSON request (still need to hack the ORM a bit to generate associative lists from records so they play nicely with HStringTemplate).

--- LulzBB/Forum.hs
+++ LulzBB/Forum.hs
@@ -1,15 +1,20 @@
 module LulzBB.Forum where
 import Control.Monad (liftM)
+import Data.Maybe (fromMaybe)
 import Database.HDBC
 import LulzBB.ORM
 import Routing (RouteParameters)
+import Text.JSON (encode)
 
 list :: IConnection a => a -> RouteParameters -> IO String
 list db params = do
 	stm <- prepare db "SELECT * FROM s_forum"
 	execute stm []
-	fs <- liftM (map parseSql) $ fetchAllRowsMap stm :: IO [Maybe Forum]
-	return $ show fs
+	fs <- liftM (map (fromMaybe undefined . parseSql)) $ fetchAllRowsMap stm :: IO [Forum]
+
+	case lookup "format" params of
+		Just "json" -> return $ encode fs
+		_ -> undefined
 
 view :: IConnection a => a -> RouteParameters -> IO String
 view db params = return "view forum"
