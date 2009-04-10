d70a19140fe3da2805cf3a0da4bc8b1ad58826da
taro <qudesu@gmail.com>
2009-04-10 11:46:18 -0400

    Added some Haddock to the View.

--- View.hs
+++ View.hs
@@ -8,11 +8,21 @@ import Data.Maybe (catMaybes)
 import Text.Regex.Posix ((=~))
 import Text.StringTemplate (getStringTemplate, STGroup, StringTemplate)
 
+{-|
+	Function which converts the input string to lowercase, then
+	strips out all of the non [a-z_] characters to make template
+	names easier to handle.
+-}
 formatSuggPart :: String -> String
 formatSuggPart = 
 	strip . map toLower
-		where strip = concat . (=~ "[a-z]")
+		where strip = concat . (=~ "[_a-z]")
 
+{-|
+	Takes the names of the module, action, section and ID of the 
+	piece being rendered and returns a list of possible template
+	names for rendering the object.
+-}
 suggestions :: String -> String -> String -> Integer -> [String]
 suggestions mod act sec id =
 	let sid = show id in
@@ -28,6 +38,12 @@ suggestions mod act sec id =
 		lsec ++ "-" ++ ".tpl"
 	]
 
+{-|
+	Takes a STGroup and a list of template suggestions and returns a 
+	single StringTemplate (the first one in the suggestion list with
+	a corresponding template. If there is no matching template it 
+	will raise an error.
+-}
 getTemplate :: STGroup String -> [String] -> StringTemplate String
 getTemplate tpls suggs = 
 	let tpls' = catMaybes $ map (\x -> getStringTemplate x tpls) suggs in
